88089e5a2fe167590923817362be0e1fbac8e90d
Jwhyee <kawnsdud@naver.com>
2022-08-26 14:03:17 +0900

    Add getQslUser method and getQslUser implement

--- src/main/java/com/ll/exam/app3/SiteUserRepositoryImpl.java
+++ src/main/java/com/ll/exam/app3/SiteUserRepositoryImpl.java
@@ -1,5 +1,20 @@
 package com.ll.exam.app3;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import static com.ll.exam.app3.QSiteUser.siteUser;
+
+@RequiredArgsConstructor
 public class SiteUserRepositoryImpl implements SiteUserRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public SiteUser getQslUser(Long id) {
+        return jpaQueryFactory.selectFrom(siteUser)
+                .where(siteUser.id.eq(id))
+                .fetchOne();
+    }
 
 }
