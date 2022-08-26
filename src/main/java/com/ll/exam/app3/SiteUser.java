9533fc79768eb9bfdbdb6901f3cffc55dd898a90
Jwhyee <kawnsdud@naver.com>
2022-08-26 13:17:34 +0900

    Change java version

--- src/main/java/com/ll/exam/app3/SiteUser.java
+++ src/main/java/com/ll/exam/app3/SiteUser.java
@@ -3,6 +3,8 @@ package com.ll.exam.app3;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -12,6 +14,7 @@ import javax.persistence.Id;
 public class SiteUser {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
